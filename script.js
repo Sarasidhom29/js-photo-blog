69b27a8e544a15e64827472c5d85546a0a2d8149
Sara Sidhom <s.sara.29.07.2006@gmail.com>
2026-04-17 12:05:52 +0200

    stesura iniziale fetch

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+// @ts-check
 'use strict';
+
+const API_URL = 'https://lanciwebb.github.io/demo/api/pictures/';
+
+fetch(API_URL)
+    .then(response => {
+        return response.json();
+    })
+    .then(json => {
+        console.log(json);
+    })
+    .catch(error => {
+        console.error(error);    
+    })
+    
