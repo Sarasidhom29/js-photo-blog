ee35549707a14024fc331fed291e5ce903559b43
Sara Sidhom <s.sara.29.07.2006@gmail.com>
2026-04-17 12:52:51 +0200

    aggiunti messaggi di caricamento e di errore e creati container delle card in html

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
-// @ts-check
+ //@ts-check
 'use strict';
 
-const API_URL = 'https://lanciwebb.github.io/demo/api/pictures/';
+const API_URL = 'https://lanciweb.github.io/demo/api/pictures/';
 
-fetch(API_URL)
+
+const msgCaricamento = document.querySelector('#msg-caricamento');
+const msgErrore = document.querySelector('#msg-errore');
+
+
+if(msgCaricamento !== null && msgErrore !== null){
+    msgCaricamento.innerHTML ='Caricamento';
+    
+    fetch(API_URL)
     .then(response => {
         return response.json();
     })
     .then(json => {
         console.log(json);
+        
     })
     .catch(error => {
-        console.error(error);    
+        console.error(error); 
+        msgErrore.innerHTML = 'Si è verificato un errore';  
     })
+    .finally(() => {
+        msgCaricamento.innerHTML = '';
+    })
+}
     
