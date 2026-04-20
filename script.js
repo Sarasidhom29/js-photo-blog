dd5fa7e89a31fe68826c41b1aef4d471dc916312
Sara Sidhom <s.sara.29.07.2006@gmail.com>
2026-04-20 17:47:56 +0200

    creata funzione per creazione card e sostituito console.log(json) con creaCard(json)

--- script.js
+++ script.js
@@ -1,30 +1,56 @@
- //@ts-check
+//@ts-check
 'use strict';
 
 const API_URL = 'https://lanciweb.github.io/demo/api/pictures/';
 
+//chiamata id card-container
+const cardContainer = document.querySelector('#card-container');
 
+//funzione x creazione card
+/**
+ * @param {{id: number,
+        title: string,
+        date: string,
+        url: string}[]} cards
+ */
+function creaCard(cards) {
+    let cardCreate = '';
+
+    //ciclo for of per iterazione (in base agli oggetti nell'API)
+    for (const card of cards) {
+        cardCreate += `
+        <div class="card">
+            <img src="./img/pin.svg" alt="immagine pin">
+            <img src="${card.url}" alt="immagine ${card.title}">
+            <h2> ${card.title} </h2>  
+        </div>
+        `
+    }
+    if(cardContainer !== null){
+        cardContainer.innerHTML = cardCreate;
+    }
+};
+
+//chiamata id x messaggi caricamento/errore
 const msgCaricamento = document.querySelector('#msg-caricamento');
 const msgErrore = document.querySelector('#msg-errore');
 
 
-if(msgCaricamento !== null && msgErrore !== null){
-    msgCaricamento.innerHTML ='Caricamento';
-    
+if (msgCaricamento !== null && msgErrore !== null) {
+    msgCaricamento.innerHTML = 'Caricamento';
+
     fetch(API_URL)
-    .then(response => {
-        return response.json();
-    })
-    .then(json => {
-        console.log(json);
-        
-    })
-    .catch(error => {
-        console.error(error); 
-        msgErrore.innerHTML = 'Si è verificato un errore';  
-    })
-    .finally(() => {
-        msgCaricamento.innerHTML = '';
-    })
+        .then(response => {
+            return response.json();
+        })
+        .then(json => {
+            creaCard(json);
+        })
+        .catch(error => {
+            console.error(error);
+            msgErrore.innerHTML = 'Si è verificato un errore';
+        })
+        .finally(() => {
+            msgCaricamento.innerHTML = '';
+        })
 }
-    
